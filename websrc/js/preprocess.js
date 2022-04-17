d7d55249fee67c19a363ddfebfc2982d9fe8e909
Kazutaka Matsuda <kztk@ecei.tohoku.ac.jp>
2022-04-17 16:45:38 +0900

    Copy buttons (revised CSS, and changed JS due to a compatibility issue with Firefox)

--- websrc/js/preprocess.js
+++ websrc/js/preprocess.js
@@ -95,7 +95,7 @@ function addCopyButtons() {
       -->
 
       <pre class="sourceCode">...
-        <span class="copyButton"></span>
+        <div class="code-control"><span class="copyButton"></span></div>
       </pre>      
      */
 
@@ -105,28 +105,45 @@ function addCopyButtons() {
         // if ( cs.length != 1 ) { return true; }
         // const code = cs[0]; 
 
+        const div = document.createElement('div');
+        div.classList.add('code-control');
         const copyButton = document.createElement("span");
         copyButton.classList.add('copy-button');        
+
+        const k = function() {
+            navigator.clipboard.writeText( elem.innerText ).then( function () {                        
+                // copyButton.classList.add('copied');                        
+                // setTimeout(function () {
+                //     copyButton.classList.remove('copied');
+                // }, 3000); 
+            }).catch( function(error) {
+                try {
+                    console.log("fall back to execCommand");
+                    elem.select();
+                    document.execCommand("copy");
+                }
+                catch (err) {
+                    copyButton.classList.add('copy-failed');
+                    setTimeout(function () {
+                        copyButton.classList.remove('copy-failed');
+                    }, 3000);                
+                }         
+            });
+        };
 
         copyButton.addEventListener('click', function(e) {
             navigator.permissions.query({name: "clipboard-write"}).then(result => {
                 if (result.state == "granted" || result.state == "prompt") {
-                    navigator.clipboard.writeText( elem.innerText ).then( function () {                        
-                        // copyButton.classList.add('copied');                        
-                        // setTimeout(function () {
-                        //     copyButton.classList.remove('copied');
-                        // }, 3000); 
-                    }).catch( function(error) {
-                        copyButton.classList.add('copy-failed');
-                        setTimeout(function () {
-                            copyButton.classList.remove('copy-failed');
-                        }, 3000);                         
-                    });
+                    k(); 
                 }
+            }).catch( function(error) {
+                // try without permission
+                k();
             });
         });        
 
-        elem.appendChild( copyButton ); 
+        div.appendChild( copyButton ); 
+        elem.prepend( div ); 
     });
 }
 
