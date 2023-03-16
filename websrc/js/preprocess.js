9eea967dd33b93aabc96db3827c1b3f60b48568d
Kazutaka Matsuda <kztk@ecei.tohoku.ac.jp>
2023-03-16 14:57:25 +0900

    Fixed #18

--- websrc/js/preprocess.js
+++ websrc/js/preprocess.js
@@ -1,21 +1,22 @@
 function addClassToCurrentPageLinks() {
     const currentURL = document.URL.split("/");
-    const currentPage = currentURL[ currentURL.length - 1 ];
+    const currentPage = currentURL[currentURL.length - 1];
 
-    // We assume that the menu list are shallow
+    const p = currentPage.replace(/\?.+/, "").replace(/#.+/, "");
 
-    document.querySelectorAll('a').forEach( elem => {
+    // We assume that the page structure is shallow
+    document.querySelectorAll('a').forEach(elem => {
         const tURL = elem.href.split("/");
-        const tPage = tURL[ tURL.length - 1 ];
-        
-        if ( tPage == currentPage ) {
+        const tPage = tURL[tURL.length - 1];
+
+        if (tPage == p) {
             elem.className += ' current-link';
         }
     });
 }
 
 function addModalImageViewPane() {
-    const mainElem = document.getElementsByTagName("body")[0]; 
+    const mainElem = document.getElementsByTagName("body")[0];
 
     // <div id="modalPane" class="modal-pane">
     //     <span class="close-button">&times;</span>
@@ -25,36 +26,36 @@ function addModalImageViewPane() {
     const div = document.createElement("div");
     div.setAttribute("id", "modalPane");
     div.setAttribute("class", "modal-pane");
-    
+
     const img = document.createElement("img");
     img.setAttribute("id", "modalImage");
     img.setAttribute("class", "modal-image");
 
     const closeButton = document.createElement("span");
     closeButton.setAttribute("class", "close-button");
-    closeButton.appendChild( document.createTextNode("") );
+    closeButton.appendChild(document.createTextNode(""));
     closeButton.onclick = function () {
         div.style.display = "none";
     };
 
-    div.addEventListener('click', function(event) {
-        if ( !img.contains( event.target ) ) {
-            div.style.display = "none"; 
-        }        
+    div.addEventListener('click', function (event) {
+        if (!img.contains(event.target)) {
+            div.style.display = "none";
+        }
     });
-    
-    div.appendChild( closeButton ); 
-    div.appendChild( img ); 
 
-    mainElem.appendChild( div ); 
+    div.appendChild(closeButton);
+    div.appendChild(img);
 
-    document.querySelectorAll('img').forEach( elem => {
+    mainElem.appendChild(div);
+
+    document.querySelectorAll('img').forEach(elem => {
         if (elem.clientHeight < elem.naturalHeight) {
             elem.className += ' enlargeable'
             elem.onclick = function () {
                 div.style.display = "block";
                 img.src = elem.src;
-            };        
+            };
         }
     });
 }
@@ -62,18 +63,18 @@ function addModalImageViewPane() {
 function addTouchEventHadnlingToDD() {
     const dds = document.querySelectorAll('.dd');
     // FIXME: Just adding 'touchstart' would be enough for iPad 
-    dds.forEach( elem => {
-        elem.addEventListener('touchstart', function(event) {
-            if ( elem.classList.contains('touched') ) {
+    dds.forEach(elem => {
+        elem.addEventListener('touchstart', function (event) {
+            if (elem.classList.contains('touched')) {
                 elem.classList.remove('touched');
             }
             else {
-                dds.forEach( elem => {
+                dds.forEach(elem => {
                     elem.classList.remove('touched');
                 });
                 elem.classList.add('touched');
             }
-        });        
+        });
     });
 }
 
@@ -85,10 +86,10 @@ function addCopyButtons() {
       
       for code blocks.       
 
-     */    
-    const codeBlocks = document.querySelectorAll('pre.sourceCode'); 
+     */
+    const codeBlocks = document.querySelectorAll('pre.sourceCode');
 
-    
+
     /*
       <pre class="sourceCode">...</pre>
 
@@ -99,24 +100,24 @@ function addCopyButtons() {
       </pre>      
      */
 
-    codeBlocks.forEach (elem => {
+    codeBlocks.forEach(elem => {
         // const cs = elem.querySelectorAll('code');
-        
+
         // if ( cs.length != 1 ) { return true; }
         // const code = cs[0]; 
 
         const div = document.createElement('div');
         div.classList.add('code-control');
         const copyButton = document.createElement("span");
-        copyButton.classList.add('copy-button');        
+        copyButton.classList.add('copy-button');
 
-        const k = function() {
-            navigator.clipboard.writeText( elem.innerText ).then( function () {                        
+        const k = function () {
+            navigator.clipboard.writeText(elem.innerText).then(function () {
                 // copyButton.classList.add('copied');                        
                 // setTimeout(function () {
                 //     copyButton.classList.remove('copied');
                 // }, 3000); 
-            }).catch( function(error) {
+            }).catch(function (error) {
                 try {
                     console.log("fall back to execCommand");
                     elem.select();
@@ -126,24 +127,24 @@ function addCopyButtons() {
                     copyButton.classList.add('copy-failed');
                     setTimeout(function () {
                         copyButton.classList.remove('copy-failed');
-                    }, 3000);                
-                }         
+                    }, 3000);
+                }
             });
         };
 
-        copyButton.addEventListener('click', function(e) {
-            navigator.permissions.query({name: "clipboard-write"}).then(result => {
+        copyButton.addEventListener('click', function (e) {
+            navigator.permissions.query({ name: "clipboard-write" }).then(result => {
                 if (result.state == "granted" || result.state == "prompt") {
-                    k(); 
+                    k();
                 }
-            }).catch( function(error) {
+            }).catch(function (error) {
                 // try without permission
                 k();
             });
-        });        
+        });
 
-        div.appendChild( copyButton ); 
-        elem.prepend( div ); 
+        div.appendChild(copyButton);
+        elem.prepend(div);
     });
 }
 
