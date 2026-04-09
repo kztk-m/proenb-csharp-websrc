80ccf82bba0502ac9dd6e4771cc1b2ae834cef5f
Kazutaka Matsuda <kztk@tohoku.ac.jp>
2026-04-09 15:25:35 +0900

    Formatter

--- gen/site.hs
+++ gen/site.hs
@@ -1,71 +1,73 @@
 --------------------------------------------------------------------------------
-{-# LANGUAGE MultiWayIf        #-}
+{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE OverloadedStrings #-}
-import           Hakyll
 
-import qualified Data.Time             as Time
+import Hakyll
 
-import qualified System.Directory      as Dir
-import qualified System.FilePath       as FP
-import qualified Text.Pandoc           as P
-import           Text.Pandoc           (ReaderOptions (..), WriterOptions (..))
+import qualified Data.Time as Time
+
+import qualified System.Directory as Dir
+import qualified System.FilePath as FP
+import Text.Pandoc (ReaderOptions (..), WriterOptions (..))
+import qualified Text.Pandoc as P
 import qualified Text.Pandoc.Shared
-import qualified Text.Pandoc.Walk      as P
+import qualified Text.Pandoc.Walk as P
 
 import qualified Data.Char
-import           Data.Functor.Identity (runIdentity)
-import           Data.String           (fromString)
-import           Data.Text             (Text)
-import qualified Data.Text             as T
+import Data.Functor.Identity (runIdentity)
+import Data.String (fromString)
+import Data.Text (Text)
+import qualified Data.Text as T
 
-import           Control.Category      ((>>>))
-import qualified Data.List             (isPrefixOf)
+import Control.Category ((>>>))
+import qualified Data.List (isPrefixOf)
 
 -- import qualified Debug.Trace
 
 conf :: Configuration
-conf = defaultConfiguration
-       {
-         deployCommand = "cd .. && sh ./deploy.sh",
-         providerDirectory = "../websrc",
-         destinationDirectory = "../_site",
-         storeDirectory       = "../_cache",
-         tmpDirectory         = "../_cache/tmp"
-       }
+conf =
+  defaultConfiguration
+    { deployCommand = "cd .. && sh ./deploy.sh"
+    , providerDirectory = "../websrc"
+    , destinationDirectory = "../_site"
+    , storeDirectory = "../_cache"
+    , tmpDirectory = "../_cache/tmp"
+    }
 
 myReaderOptions :: ReaderOptions
 myReaderOptions =
   defaultHakyllReaderOptions
-  {
-    -- No smart quotes
-    readerExtensions = P.disableExtension P.Ext_smart (readerExtensions defaultHakyllReaderOptions),
-    readerColumns = 10000 -- suppress Markdown to determine the proportion of table columns
-  }
+    { -- No smart quotes
+      readerExtensions = P.disableExtension P.Ext_smart (readerExtensions defaultHakyllReaderOptions)
+    , readerColumns = 10000 -- suppress Markdown to determine the proportion of table columns
+    }
 
 myWriterOptions :: WriterOptions
 myWriterOptions =
   defaultHakyllWriterOptions
-  {
-    writerExtensions = P.disableExtension P.Ext_smart (writerExtensions defaultHakyllWriterOptions), 
-    writerSectionDivs = True 
-  }
+    { writerExtensions = P.disableExtension P.Ext_smart (writerExtensions defaultHakyllWriterOptions)
+    , writerSectionDivs = True
+    }
 
 -- from: https://svejcar.dev/posts/2019/11/27/table-of-contents-in-hakyll/
 withTOC :: WriterOptions
-withTOC = myWriterOptions
-          { -- writerNumberSections  = True,
-            writerTableOfContents = True,
-            writerTOCDepth        = 3,
-            writerTemplate        = Just tocTemplate
-          }
+withTOC =
+  myWriterOptions
+    { -- writerNumberSections  = True,
+      writerTableOfContents = True
+    , writerTOCDepth = 3
+    , writerTemplate = Just tocTemplate
+    }
 
 tocTemplate :: P.Template Text
-tocTemplate = either error id . runIdentity . P.compileTemplate "" $ T.unlines
-  [ "<nav class=\"toc\">"
-  , "$toc$"
-  , "</nav>"
-  , "<section class=\"mainbody\">$body$</section>"
-  ]
+tocTemplate =
+  either error id . runIdentity . P.compileTemplate "" $
+    T.unlines
+      [ "<nav class=\"toc\">"
+      , "$toc$"
+      , "</nav>"
+      , "<section class=\"mainbody\">$body$</section>"
+      ]
 
 data ParseBN = W Int | Q Int
 
@@ -84,52 +86,52 @@ adjustJapaneseSpacing = pure . fmap aj
 
     aj :: String -> String
     aj [] = []
-    aj ('<':'/':'e':'m':'>':sp:c:cs) | Data.Char.isSpace sp && isJpChar c = "</em>" ++ aj (c:cs)
-    aj ('<':'/':'a':'>':sp:c:cs) | Data.Char.isSpace sp && isJpChar c = "</a>" ++ aj (c:cs)
-    aj ('<':'/':'s':'t':'r':'o':'n':'g':'>':sp:c:cs) | Data.Char.isSpace sp && isJpChar c = "</strong>" ++ aj (c:cs)
-    aj ('<':'p':'r':'e':cs) = "<pre" ++ ajPre cs
-    aj (c:cs)
-     | isJpChar c = ajAfterJp c cs
-     | otherwise  = c:aj cs
+    aj ('<' : '/' : 'e' : 'm' : '>' : sp : c : cs) | Data.Char.isSpace sp && isJpChar c = "</em>" ++ aj (c : cs)
+    aj ('<' : '/' : 'a' : '>' : sp : c : cs) | Data.Char.isSpace sp && isJpChar c = "</a>" ++ aj (c : cs)
+    aj ('<' : '/' : 's' : 't' : 'r' : 'o' : 'n' : 'g' : '>' : sp : c : cs) | Data.Char.isSpace sp && isJpChar c = "</strong>" ++ aj (c : cs)
+    aj ('<' : 'p' : 'r' : 'e' : cs) = "<pre" ++ ajPre cs
+    aj (c : cs)
+      | isJpChar c = ajAfterJp c cs
+      | otherwise = c : aj cs
 
-    ajPre []                           = []
-    ajPre ('<':'/':'p':'r':'e':'>':cs) = "</pre>" ++ aj cs
-    ajPre (c:cs)                       = c:ajPre cs
+    ajPre [] = []
+    ajPre ('<' : '/' : 'p' : 'r' : 'e' : '>' : cs) = "</pre>" ++ aj cs
+    ajPre (c : cs) = c : ajPre cs
 
     ajAfterJp :: Char -> String -> String
     ajAfterJp jc [] = [jc]
-    ajAfterJp jc ('\n':cs) | Just rest <- skipTrailingNBSPBeforeJp cs = jc:aj rest
-    ajAfterJp jc (' ':'<':cs) | "a " `Data.List.isPrefixOf` cs || "strong" `Data.List.isPrefixOf` cs || "em" `Data.List.isPrefixOf` cs = jc:"<"++aj cs
-    ajAfterJp jc (c:cs) = jc:aj(c:cs)
+    ajAfterJp jc ('\n' : cs) | Just rest <- skipTrailingNBSPBeforeJp cs = jc : aj rest
+    ajAfterJp jc (' ' : '<' : cs) | "a " `Data.List.isPrefixOf` cs || "strong" `Data.List.isPrefixOf` cs || "em" `Data.List.isPrefixOf` cs = jc : "<" ++ aj cs
+    ajAfterJp jc (c : cs) = jc : aj (c : cs)
 
     skipTrailingNBSPBeforeJp :: String -> Maybe String
     skipTrailingNBSPBeforeJp [] = Just []
-    skipTrailingNBSPBeforeJp ('\n':_) = Nothing
-    skipTrailingNBSPBeforeJp ('\r':_) = Nothing
-    skipTrailingNBSPBeforeJp (c:cs)
-     | Data.Char.isSpace c = skipTrailingNBSPBeforeJp cs
-     | isJpChar c = Just (c:cs)
-     | otherwise = Nothing
+    skipTrailingNBSPBeforeJp ('\n' : _) = Nothing
+    skipTrailingNBSPBeforeJp ('\r' : _) = Nothing
+    skipTrailingNBSPBeforeJp (c : cs)
+      | Data.Char.isSpace c = skipTrailingNBSPBeforeJp cs
+      | isJpChar c = Just (c : cs)
+      | otherwise = Nothing
 
 procMarkdownAdmonitions :: P.Pandoc -> P.Pandoc
 procMarkdownAdmonitions = P.walk go
   where
-    splitAtSoftbreak :: [P.Inline] -> ([P.Inline],[P.Inline])
+    splitAtSoftbreak :: [P.Inline] -> ([P.Inline], [P.Inline])
     splitAtSoftbreak [] = ([], [])
-    splitAtSoftbreak (i:is) = case i of
+    splitAtSoftbreak (i : is) = case i of
       P.SoftBreak -> ([], is)
-      _           -> let (s, t) = splitAtSoftbreak is in (i:s, t)
+      _ -> let (s, t) = splitAtSoftbreak is in (i : s, t)
 
     go :: P.Block -> P.Block
-    go (P.BlockQuote (P.Para para:bs2)) -- too specific?
-      | ([P.Str "[!NOTE]"],bs1) <- splitAtSoftbreak para = mkDiv "note" "Note" (P.Para bs1 : bs2)
-      | ([P.Str "[!TIP]"],bs1) <- splitAtSoftbreak para = mkDiv "tip" "Tip" (P.Para bs1 : bs2)
-      | ([P.Str "[!CAUTION]"],bs1) <- splitAtSoftbreak para= mkDiv "caution" "Caution" (P.Para bs1 : bs2)
-      | ([P.Str "[!IMPORTANT]"],bs1) <- splitAtSoftbreak para = mkDiv "important" "Important" (P.Para bs1 : bs2)
-      | ([P.Str "[!WARNING]"],bs1) <- splitAtSoftbreak para = mkDiv "warning" "Warning" (P.Para bs1 : bs2)
+    go (P.BlockQuote (P.Para para : bs2)) -- too specific?
+      | ([P.Str "[!NOTE]"], bs1) <- splitAtSoftbreak para = mkDiv "note" "Note" (P.Para bs1 : bs2)
+      | ([P.Str "[!TIP]"], bs1) <- splitAtSoftbreak para = mkDiv "tip" "Tip" (P.Para bs1 : bs2)
+      | ([P.Str "[!CAUTION]"], bs1) <- splitAtSoftbreak para = mkDiv "caution" "Caution" (P.Para bs1 : bs2)
+      | ([P.Str "[!IMPORTANT]"], bs1) <- splitAtSoftbreak para = mkDiv "important" "Important" (P.Para bs1 : bs2)
+      | ([P.Str "[!WARNING]"], bs1) <- splitAtSoftbreak para = mkDiv "warning" "Warning" (P.Para bs1 : bs2)
       where
-        mkDiv c t bs = P.Div ("",[c],[]) $ P.Div ("",["title"],[]) [P.Para [P.Str t]] : bs
-    go b                                      = b
+        mkDiv c t bs = P.Div ("", [c], []) $ P.Div ("", ["title"], []) [P.Para [P.Str t]] : bs
+    go b = b
 
 myTransformM :: P.Pandoc -> Compiler P.Pandoc
 myTransformM d = do
@@ -139,14 +141,13 @@ myTransformM d = do
     then pure $ procMarkdownAdmonitions d'
     else pure d'
 
-
 pageCompilingRule :: Compiler (Item String)
 pageCompilingRule = do
   underlying <- getUnderlying
-  toc        <- getMetadataField underlying "tableOfContents"
+  toc <- getMetadataField underlying "tableOfContents"
   let wopt = case toc of
-               Just "true" -> withTOC
-               _           -> myWriterOptions
+        Just "true" -> withTOC
+        _ -> myWriterOptions
 
   fp <- getResourceFilePath
   mt <- getItemModificationTime underlying
@@ -158,8 +159,8 @@ pageCompilingRule = do
           (Time.utcToLocalTime (Time.TimeZone (9 * 60) False "JST") utc)
 
   let ctxt =
-        constField "lastModified" (formatTimeAsJST mt) <>
-        defaultContext
+        constField "lastModified" (formatTimeAsJST mt)
+          <> defaultContext
 
   let bn = FP.takeBaseName fp
   let postProc a = relativizeUrls a >>= adjustJapaneseSpacing
@@ -167,103 +168,98 @@ pageCompilingRule = do
   case checkBN bn of
     Just (Q i) -> do
       inst <-
-        if i <= 4 then load "questions/instruction.md"
-        else           load "questions/eto_instruction.md"
+        if i <= 4
+          then load "questions/instruction.md"
+          else load "questions/eto_instruction.md"
 
       mti <- getItemModificationTime (itemIdentifier inst)
 
       let ctxt' =
-            constField "instruction" (itemBody inst) <>
-            constField "number"      (show i) <>
-            constField "lastModified" (formatTimeAsJST $ max mt mti) <> -- update lastModified
-            ctxt
-
+            constField "instruction" (itemBody inst)
+              <> constField "number" (show i)
+              <> constField "lastModified" (formatTimeAsJST $ max mt mti)
+              <> ctxt -- update lastModified
       pandocCompilerWithTransformM myReaderOptions wopt myTransformM
         >>= loadAndApplyTemplate "templates/defaultQ.html" ctxt'
         >>= postProc
-
     Just (W i) -> do
       let ctxt' =
-            constField "number"      (show i) <>
-            ctxt
+            constField "number" (show i)
+              <> ctxt
 
       pandocCompilerWithTransformM myReaderOptions wopt myTransformM
         >>= loadAndApplyTemplate "templates/defaultW.html" ctxt'
         >>= postProc
-
-
     Nothing -> do
       let tmpl =
-            if "setup" `Data.List.isPrefixOf` bn then
-              "templates/defaultSetup.html"
-            else
-              "templates/default.html"
+            if "setup" `Data.List.isPrefixOf` bn
+              then
+                "templates/defaultSetup.html"
+              else
+                "templates/default.html"
       pandocCompilerWithTransformM myReaderOptions wopt myTransformM
         >>= loadAndApplyTemplate tmpl ctxt
         >>= postProc
   where
     checkBN :: String -> Maybe ParseBN
-    checkBN (c:s) =
-      case [ n | (n, []) <- reads s ] of
+    checkBN (c : s) =
+      case [n | (n, []) <- reads s] of
         [n] ->
-          if | c == 'w'  -> Just (W n)
-             | c == 'q'  -> Just (Q n)
-             | otherwise -> Nothing
-        _   -> Nothing
+          if
+            | c == 'w' -> Just (W n)
+            | c == 'q' -> Just (Q n)
+            | otherwise -> Nothing
+        _ -> Nothing
     checkBN _ = Nothing
-
-
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyllWith conf $ do
-    match "images/**" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "images/**" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "files/**" $ do 
-        route $ gsubRoute "files/" (const "")
-        compile copyFileCompiler
+  match "files/**" $ do
+    route $ gsubRoute "files/" (const "")
+    compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
 
-    match "js/*" $ do
-      route idRoute
-      compile copyFileCompiler
+  match "js/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "templates/*" $
-      compile $ do
-        templateBodyCompiler
+  match "templates/*" $
+    compile $ do
+      templateBodyCompiler
 
-    match "questions/*" $ do
-      compile $ pandocCompilerWithTransformM myReaderOptions myWriterOptions myTransformM
+  match "questions/*" $ do
+    compile $ pandocCompilerWithTransformM myReaderOptions myWriterOptions myTransformM
 
-    match "pages/*.md" $ do
-      route $ gsubRoute "pages/" (const "") `composeRoutes` setExtension "html"
-      compile pageCompilingRule
+  match "pages/*.md" $ do
+    route $ gsubRoute "pages/" (const "") `composeRoutes` setExtension "html"
+    compile pageCompilingRule
 
-    match "pages/*.rst" $ do
-      route $ gsubRoute "pages/" (const "") `composeRoutes` setExtension "html"
-      compile pageCompilingRule
+  match "pages/*.rst" $ do
+    route $ gsubRoute "pages/" (const "") `composeRoutes` setExtension "html"
+    compile pageCompilingRule
 
-    match "pages/*.html" $ do
-      route $ gsubRoute "pages/" (const "")
-      compile $
-        getResourceBody
-        >>= relativizeUrls >>= adjustJapaneseSpacing
+  match "pages/*.html" $ do
+    route $ gsubRoute "pages/" (const "")
+    compile $
+      getResourceBody
+        >>= relativizeUrls
+        >>= adjustJapaneseSpacing
 
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      let indexCtx = defaultContext
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-          let indexCtx = defaultContext
-
-          getResourceBody
-            >>= applyAsTemplate indexCtx
-            >>= loadAndApplyTemplate "templates/default.html" indexCtx
-            >>= relativizeUrls >>= adjustJapaneseSpacing
-
-
-
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
+        >>= adjustJapaneseSpacing
